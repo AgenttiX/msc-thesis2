2c5261d12a8a308c7370bf76a9cee17e3ed9f042
Mika Mäki <agentti002x@gmail.com>
2023-01-17 14:48:34 +0200

    Expand test script

--- msc2-python/energy_fraction.py
+++ msc2-python/energy_fraction.py
@@ -1,13 +1,22 @@
-import giese_lisa
+import numpy as np
+
+from giese import lisa
 
 
 def main():
-    cs2b = 1/3
     cs2s = 1/3
-    al = 0.1
-    vw = 0.5
-    kappa = giese_lisa.kappaNuMuModel(cs2b, cs2s, al, vw)
-    print(kappa)
+    cs2b = (1/np.sqrt(3) - 0.01)**2
+    alpha_ns = [0.578, 0.151, 0.1]
+    v_walls = [0.5, 0.7, 0.8]
+
+    # cs2s = 1/3
+    # cs2b = 1/3
+    # alpha_ns = [0.578, 0.151, 0.091]
+    # v_walls = [0.5, 0.7, 0.77]
+
+    for alpha_n, v_wall in zip(alpha_ns, v_walls):
+        kappa = lisa.kappaNuMuModel(cs2b, cs2s, alpha_n, v_wall)
+        print(kappa)
 
 
 if __name__ == "__main__":
