6090c3f971595a745cbe559d3e5a36a38005666d
Mika Mäki <agentti002x@gmail.com>
2023-01-19 15:07:23 +0200

    Add PTtools loader

---
+++ msc2-python/loader.py
@@ -0,0 +1,16 @@
+"""Add PTtools to PATH
+
+This can be done directly in PyCharm by going to Settings -> Project -> Project Structure
+"""
+
+import os.path
+import sys
+
+REPO_DIR = os.path.dirname(os.path.abspath(__file__))
+PTTOOLS_DIR = os.path.join(os.path.dirname(os.path.dirname(REPO_DIR)), "pttools")
+if not os.path.isdir(PTTOOLS_DIR):
+    raise NotADirectoryError(f"PTtools directory not found: {PTTOOLS_DIR}")
+
+
+def load():
+    sys.path.insert(1, PTTOOLS_DIR)
