b40c9264504ca14abf51cf997905af06f9be0981
Mika Mäki <agentti002x@gmail.com>
2023-05-04 16:56:01 +0300

    Improve Tampere slides

---
+++ tampere_seminar_presentation/sin_transform_core.py
@@ -0,0 +1,7 @@
+<@\textcolor{red}{@numba.njit(parallel=True)}@>
+def sin_transform_core(t: np.ndarray, f: np.ndarray, freq: np.ndarray) -> np.ndarray:
+    integral = np.zeros_like(freq)
+    for i in <@\textcolor{red}{numba.prange(freq.size)}@>:
+        integrand = f * np.sin(freq[i] * t)
+        integral[i] = np.trapz(integrand, t)
+    return integral
